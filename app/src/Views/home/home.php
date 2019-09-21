e28f71b6e784ea4fbcc8c10d085d31313a861bf6
fassumpcao <felipe.p.assumpcao@gmail.com>
2019-09-21 18:15:30 -0300

    Ajuste na página home.

--- app/src/Views/home/home.php
+++ app/src/Views/home/home.php
@@ -4,32 +4,31 @@ if (! defined('URL')) {
     exit();
 }
 
-var_dump($this->Dados);
+if (isset($this->Dados)) {
 
-/*
-    //**************
-    //Transforma os campos do array em variaveis
-    //**************
 
-    extract(json_decode($this->Dados,true));
-    echo $nome;
+    /*
+        //*-------------------------*
+        //Transforma os campos do array em variaveis
+        //*-------------------------*
+        extract(json_decode($this->Dados,true));
+        echo "variavel nome=" . $nome;
 
-*/
+    */
 
-    //**************
-    //Transforma os campos do array em objetos
-    //**************
-
-$dados = json_decode($this->Dados,false);
-echo $dados->nome;
-
+        //*-------------------------*
+        //Transforma os campos do array em objetos
+        //*-------------------------*
+        $dados = json_decode(json_encode($this->Dados), false);
+        var_dump($dados);
+}
 ?>
 
 
 <main class="container destaque">
 
     <section class="busca">
-        <h2>Busca</h2>
+        <h2>Enviar Post</h2>
 
         <form method="POST" action="">
             <div class="form-group col-md-6">
